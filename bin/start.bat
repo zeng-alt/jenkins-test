b824a74251a9026dde8f9f314194f58311c66b7e
zengJiaJun <zengjiazun@chinasie.com>
2025-07-23 14:29:28 +0800

    更新启动脚本

--- bin/start.bat
+++ bin/start.bat
@@ -1,3 +1,14 @@
 @echo off
 echo Starting Java application...
-java -jar .\target\jenkins-test-0.0.1-SNAPSHOT.jar
+:: 启动新的 java 应用，后台运行，并获取新进程PID
+start "" java -jar .\target\jenkins-test-0.0.1-SNAPSHOT.jar
+
+:: 这里我们没有直接方法获取新进程PID，需用一点技巧
+:: 下面示例通过找端口占用的 PID，假设你用 8080 端口
+
+timeout /t 20 /nobreak >nul
+set PID_FILE=app.pid
+for /f "tokens=5" %%a in ('netstat -ano ^| find ":9000" ^| find "LISTENING"') do (
+    echo %%a > %PID_FILE%
+    echo Saved new PID %%a to %PID_FILE%
+)
